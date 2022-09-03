7f4569c7219106cb971c9e44917bd07e1100f210
Michal <michalsidzej@gmail.com>
2022-09-03 12:56:00 +0200

    Add getWithdrawableAmount tests

--- packages/contracts/test/Campaign.js
+++ packages/contracts/test/Campaign.js
@@ -12,30 +12,51 @@ describe("CampaignCreator", function() {
     return campaignCreator
   }
 
+  async function createCampaign(campaignCreator, payout = 10) {
+    const ONE_HOUR_IN_SECS = 60 * 60
+    const [parent, student] = await ethers.getSigners()
+
+    const timestamp = await time.latest()
+
+    await campaignCreator.createCampaign(
+      student.address,
+      100,
+      timestamp + ONE_HOUR_IN_SECS,
+      payout
+    )
+
+    const campaign = await campaignCreator.ownerToCampaignMapping(
+      parent.address,
+      0
+    )
+    const campaignId = campaign[0]
+
+    return campaignId
+  }
+
   describe("getWithdrawableAmount", async function() {
     it("returns 0 when no solutions", async function() {
-      const ONE_HOUR_IN_SECS = 60 * 60
-
       const campaignCreator = await deployCampaignCreator()
-      const [parent, student] = await ethers.getSigners()
-      const timestamp = await time.latest()
+      const campaignId = await createCampaign(campaignCreator)
 
-      await campaignCreator.createCampaign(
-        student.address,
-        100,
-        timestamp + ONE_HOUR_IN_SECS,
-        10
-      )
-
-      const campaign = await campaignCreator.ownerToCampaignMapping(
-        parent.address,
-        0
-      )
-      const campaignId = campaign[0]
+      const [_, student] = await ethers.getSigners()
 
       await expect(
         await campaignCreator.connect(student).getWithdrawableAmount(campaignId)
       ).to.equal(0)
+    })
+
+    it("return payout when 1 solution", async function() {
+      const campaignCreator = await deployCampaignCreator()
+      const payout = 10
+      const campaignId = await createCampaign(campaignCreator, payout)
+
+      const [_, student] = await ethers.getSigners()
+      campaignCreator.connect(student).submitSolution(campaignId, "")
+
+      await expect(
+        await campaignCreator.connect(student).getWithdrawableAmount(campaignId)
+      ).to.equal(payout)
     })
   })
 })
