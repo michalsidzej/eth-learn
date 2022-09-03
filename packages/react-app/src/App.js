8181a29595ee0bc8bb473fa68434b15c3f0c82e0
Michal <michalsidzej@gmail.com>
2022-09-03 17:01:32 +0200

    Add main component

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,80 +1,17 @@
 import { useQuery } from "@apollo/client"
-import { Contract } from "@ethersproject/contracts"
-import {
-  shortenAddress,
-  useCall,
-  useEthers,
-  useLookupAddress,
-} from "@usedapp/core"
-import React, { useEffect, useState } from "react"
+import { useEthers } from "@usedapp/core"
+import React, { useEffect } from "react"
 
-import { Body, Button, Container, Header, Image, Link } from "./components"
+import { Body, Container, Header, Image } from "./components"
 import logo from "./ethereumLogo.png"
+import { WalletButton } from "./components/WalletButton"
 
-import abis from "@my-app/contracts"
-import GET_TRANSFERS from "./graphql/subgraph"
-
-function WalletButton() {
-  const [rendered, setRendered] = useState("")
-
-  const ens = useLookupAddress()
-  const { account, activateBrowserWallet, deactivate, error } = useEthers()
-
-  useEffect(() => {
-    if (ens) {
-      setRendered(ens)
-    } else if (account) {
-      setRendered(shortenAddress(account))
-    } else {
-      setRendered("")
-    }
-  }, [account, ens, setRendered])
-
-  useEffect(() => {
-    if (error) {
-      console.error("Error while connecting wallet:", error.message)
-    }
-  }, [error])
-
-  return (
-    <Button
-      onClick={() => {
-        if (!account) {
-          activateBrowserWallet()
-        } else {
-          deactivate()
-        }
-      }}
-    >
-      {rendered === "" && "Connect Wallet"}
-      {rendered !== "" && rendered}
-    </Button>
-  )
+function Main() {
+  return <p>Main</p>
 }
 
 function App() {
-  // Read more about useDapp on https://usedapp.io/
-  const { error: contractCallError, value: tokenBalance } =
-    useCall({
-      contract: new Contract(addresses.ceaErc20, abis.erc20),
-      method: "balanceOf",
-      args: ["0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C"],
-    }) ?? {}
-
-  const { loading, error: subgraphQueryError, data } = useQuery(GET_TRANSFERS)
-
-  useEffect(() => {
-    if (subgraphQueryError) {
-      console.error(
-        "Error while querying subgraph:",
-        subgraphQueryError.message
-      )
-      return
-    }
-    if (!loading && data && data.transfers) {
-      console.log({ transfers: data.transfers })
-    }
-  }, [loading, subgraphQueryError, data])
+  const { account } = useEthers()
 
   return (
     <Container>
@@ -83,14 +20,7 @@ function App() {
       </Header>
       <Body>
         <Image src={logo} alt="ethereum-logo" />
-        <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
-        </p>
-        <Link href="https://reactjs.org">Learn React</Link>
-        <Link href="https://usedapp.io/">Learn useDapp</Link>
-        <Link href="https://thegraph.com/docs/quick-start">
-          Learn The Graph
-        </Link>
+        {account ? <Main /> : <p>Connect your wallet to start learning!</p>}
       </Body>
     </Container>
   )
